c10a70af98dcda4cdd31f80480dc324d2607b36a
Fatemeh <f.khalleghi.777@gmail.com>
2024-10-22 22:17:41 +0200

    Scalar vs String vs Array 2

--- src/main/java/se/lexicon/ScalarVsArrayVsString.java
+++ src/main/java/se/lexicon/ScalarVsArrayVsString.java
@@ -4,24 +4,24 @@ import java.util.Arrays;
 
 public class ScalarVsArrayVsString {
     public static void main(String[] args) {
-        System.out.println("Scalar");
+        System.out.println("Scalar:");
         scalar();
         System.out.println("-----------------------");
-        System.out.println("String");
+        System.out.println("String:");
         string();
         System.out.println("-----------------------");
-        System.out.println("Array");
+        System.out.println("Array:");
         array();
     }
 
     public static void scalar() {
         int scalar = 10;
-        System.out.println(scalar);
+        System.out.println("scalar: " + scalar);
         int referenceScalar = scalar;
-        System.out.println(referenceScalar);
+        System.out.println("ref. scalar: " + referenceScalar);
         scalar = 20;
-        System.out.println(scalar);
-        System.out.println(referenceScalar);
+        System.out.println("scalar: " + scalar);
+        System.out.println("ref. scalar:" + referenceScalar);
     }
 
     public static void string() {
@@ -37,16 +37,16 @@ public class ScalarVsArrayVsString {
 
     public static void array() {
         int[] array = {1, 2, 3, 4, 5};
-        System.out.println(array);
-        System.out.println(Arrays.toString(array));
+        System.out.println("array address: " + array);
+        System.out.println("array: " + Arrays.toString(array));
         int[] referenceArray = array;
-        System.out.println(referenceArray);
-        System.out.println(Arrays.toString(referenceArray));
+        System.out.println("ref. array address: " + referenceArray);
+        System.out.println("ref. array: " + Arrays.toString(referenceArray));
         array[0] = 100;
-        System.out.println(array);
-        System.out.println(Arrays.toString(array));
-        System.out.println(referenceArray);
-        System.out.println(Arrays.toString(referenceArray));
+        System.out.println("array address: " + array);
+        System.out.println("array: " + Arrays.toString(array));
+        System.out.println("ref. array address: " + referenceArray);
+        System.out.println("ref. array: " + Arrays.toString(referenceArray));
 
     }
 }
